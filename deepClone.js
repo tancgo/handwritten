0a445ecc6de8aa83d6c7d7749d92e65df6bb7612
tancgo <542112893@qq.com>
2020-10-02 20:17:38 +0800

    deepClone

---
+++ deepClone.js
@@ -0,0 +1,40 @@
+// 方法一 缺点是函数 symbol 等熟悉无法被拷贝
+
+var arr = [
+  function () {
+    console.log("a");
+  },
+  {
+    b: function () {
+      console.log("b");
+    },
+  },
+];
+
+var new_arr = JSON.parse(JSON.stringify(arr));
+
+console.log(new_arr);
+
+// 递归浅拷贝
+function deepClone(obj) {
+  function isObject(o) {
+    return typeof o === "object" && o !== null;
+  }
+
+  if (!isObject(obj)) {
+    throw new Error("非对象");
+  }
+
+  const newObj = obj instanceof Array ? [] : {};
+
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const cur = obj[key];
+      newObj[key] = isObject(cur) ? deepClone(cur) : cur;
+    }
+  }
+
+  return newObj;
+}
+
+console.log(deepClone(arr));
