b7304f045ace154baf6bac144ded99a22c16d3cc
tancgo <542112893@qq.com>
2020-10-19 20:26:41 +0800

    compose

--- compose.js
+++ compose.js
@@ -1,5 +1,5 @@
 // 利用 compose 将多个函数组合成一个函数，让代码从右向左运行，而不是由内而外运行，可读性大大提升。这便是函数组合。
-function compose(...funcs) {
+compose = (...funcs) => {
   // 如果参数长度为0， 则返回一个最简单的函数，即传入什么就返回什么的函数
   if (funcs.length === 0) {
     return (arg) => arg;
@@ -23,16 +23,12 @@ function compose(...funcs) {
   // });
 
   // 简化后
-  return funcs.reduce((acc, func) => (...agrs) => acc(func(...agrs)));
-}
-
-const toUpperCase = function (x) {
-  return x.toUpperCase();
-};
-const hello = function (x) {
-  return "HELLO, " + x;
+  return funcs.reduce((acc, func) => (...args) => acc(func(...args)));
 };
 
-const greet = compose(hello, toUpperCase);
+const renderName = (x) => x.toUpperCase();
+const hello = (x) => `HELLO ${x}`;
 
-greet("tancgo");
+const greet = compose(hello, renderName);
+
+console.log(greet("tancgo"));
