efbec0c2f54b4e0f23639d2e3b6b6e907c930660
tancgo <542112893@qq.com>
2020-10-22 08:44:04 +0800

    two-way-data-binding

--- two-way-data-binding/two-way-data-binding.js
+++ two-way-data-binding/two-way-data-binding.js
@@ -1,11 +1,22 @@
+/**
+ *  Proxy
+    代理的是 对象
+    可以拦截到数组的变化
+    拦截的方法多达13种
+    返回一个拦截后的数据
+     
+*   Object.defineProperty
+    代理的是属性
+    对数组数据的变化无能为力
+    直接修改原始数据
+ */
 // Object.defineProperty
-const obj = {}
-Object.defineProperty(obj, 'text', {
+const obj = {};
+Object.defineProperty(obj, "text", {
   get: function () {
-    console.log('get val');
+    console.log("get val");
   },
   set: function (newVal) {
-    console.log('set val:' + newVal);
-    
-  }
-})
+    console.log("set val:" + newVal);
+  },
+});
