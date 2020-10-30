aef574977afc07025cb2465e71e551767a813aae
tancgo <542112893@qq.com>
2020-10-30 11:38:43 +0800

    isEmpty

--- isEmpty.js
+++ isEmpty.js
@@ -1,9 +1,8 @@
 function isEmpty(obj) {
   for (const key in obj) {
-    // if (obj.hasOwnProperty(key)) {
-    //   return false;
-    // }
-    return false
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
   }
   return true;
 }
