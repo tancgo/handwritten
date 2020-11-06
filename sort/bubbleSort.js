ed38f13f008282d2b39d58bdadf0480cf523a55b
tancgo <542112893@qq.com>
2020-11-06 15:28:16 +0800

    bubble sort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,15 @@
+const arr = [5, 8, 11, 1, 78, 9, 2];
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+
+  return array;
+}
+
+console.log(bubbleSort(arr));
