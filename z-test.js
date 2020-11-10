4fef6311d8ba85971180261a236d46766205a11b
tancgo <542112893@qq.com>
2020-11-10 23:23:31 +0800

    event loop

--- z-test.js
+++ z-test.js
@@ -1,11 +1,18 @@
-function add(...args) {
-  const curied = function (...params) {
-    return add(...args.concat(params));
-  };
+Promise.resolve().then(() => {
+  setTimeout(() => {
+    console.info(4)
+  }, 0);
+  console.info(0)
+})
 
-  curied.toString = () => args.reduce((a, b) => a + b);
+setTimeout(() => {
+  console.info(1)
+}, 0);
 
-  return curied;
-}
+console.log(2);
 
-console.log(add(1)(2, 3)(4) | 0);
+Promise.resolve().then(() => {
+  console.info(3)
+})
+
+// 打印顺序 2 0 3 1 4
