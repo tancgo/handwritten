6c8b8a0925a0e17f52a50e77a9dfbaf98bfbaed3
tancgo <542112893@qq.com>
2020-11-14 14:14:26 +0800

    RegExp

--- Reg/test.js
+++ Reg/test.js
@@ -14,10 +14,56 @@ let data = {
 function render(template, data) {
   let res = template;
   for (const key in data) {
-    res = res.replace(new RegExp("\\$\\{" + key + "\\}", 'g'), data[key]);
+    res = res.replace(new RegExp("\\$\\{" + key + "\\}", "g"), data[key]);
   }
 
   return res;
 }
 
 console.log(render(template, data));
+
+// 使用正则实现手机号3 4 4 位数分隔
+const tel = "18016438098";
+
+function trans(tel) {
+  return tel
+    .replace(new RegExp("\\d{3}"), (match) => {
+      return match + " ";
+    })
+    .replace(new RegExp("\\d{4}"), (match) => {
+      return match + " ";
+    });
+}
+
+console.log(trans(tel));
+
+// 检测是否是手机号
+
+function isPhoneNumber(num) {
+  return /^1[3-9]\d{9}$/.test(num);
+}
+
+console.log(isPhoneNumber(tel));
+
+// 千分位分隔 保留三位小数  (正则不会写  写一个遍历的)
+function parseToMoney(num) {
+  const result = [];
+  let count = 0;
+  const [interger, decimal] = num.toString().split(".");
+  const nums = interger.split("");
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    count++;
+    result.unshift(nums[i]);
+    if (!(count % 3) && i !== 0) {
+      result.unshift(",");
+    }
+  }
+
+  return decimal ? result.join("") + '.' + decimal.substring(0,3) : result.join("");
+}
+
+console.log(parseToMoney(1234.56)); // return '1,234.56'
+console.log(parseToMoney(123456789)); // return '123,456,789'
+console.log(parseToMoney(1087654.321)); // return '1,087,654.321'
+console.log(parseToMoney(1087654.32112)); // return '1,087,654.321'
--- z-test.js
+++ z-test.js
@@ -1,18 +1,31 @@
-Promise.resolve().then(() => {
-  setTimeout(() => {
-    console.info(4)
-  }, 0);
-  console.info(0)
-})
+const p0 = new Promise((resolve, reject) => {
+  resolve(1);
+});
 
-setTimeout(() => {
-  console.info(1)
-}, 0);
+const p1 = new Promise((resolve, reject) => {
+  resolve(2);
+});
 
-console.log(2);
+const p2 = new Promise((resolve, reject) => {
+  reject(3);
+});
 
-Promise.resolve().then(() => {
-  console.info(3)
-})
+const p3 = new Promise((resolve, reject) => {
+  reject(4);
+});
 
-// 打印顺序 2 0 3 1 4
+const p4 = new Promise((resolve, reject) => {
+  resolve(5);
+});
+
+Promise.allSettled([p0, p1, p2, p3, p4]).then((value) => {
+  console.log(value);
+
+  let count = 0;
+
+  value.forEach(({ status }) => {
+    if (status === "rejected") count++;
+  });
+
+  console.log(count, "count");
+});
